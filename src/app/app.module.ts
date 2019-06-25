f0dcac6b74b2726e5cb4d8c67736ee3836bb727b
Jared Hook <jared_hook@hotmail.com>
2019-06-25 09:35:47 -0400

    Finish signup page, and start farm component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { FarmComponent } from './farm/farm.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  //basic routes
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: SignupPageComponent },
+  { path: 'play', component: FarmComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupPageComponent
+    SignupPageComponent,
+    FarmComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
