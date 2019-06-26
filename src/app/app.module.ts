a77dd5691b1dd382cc69e8439031ff3d6fb79fe2
Kenny Imarah <kenny.o.imarah@gmail.com>
2019-06-26 12:02:31 -0400

    26 June Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,8 +12,10 @@ import { SignupPageComponent } from './signup-page/signup-page.component';
     AppComponent,
     SignupPageComponent
   ],
-  imports: [
-    BrowserModule,
+    imports: [
+        HttpClientModule,
+        FormsModule,
+         BrowserModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup-page',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-page.component.css']
 })
 export class SignupPageComponent implements OnInit {
-
-  constructor() { }
+    
+    constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
+    onSubmit(value: any): void {
+        console.log(value.value["login"])
+        this.http.post("http://avellinfalls.com/home/add_new_user",
+            {
 
+                "login": JSON.stringify(value.value["login"])
+            })
+
+        .subscribe(
+
+            (val) => {
+
+                console.log("POST call successful value returned in body",
+
+                    val);
+               },
+              response => {
+
+                console.log("POST call in error", response);
+
+            },
+
+            () => {
+
+                console.log("The POST observable is now completed.");
+
+            });
+        }
+    
 }
