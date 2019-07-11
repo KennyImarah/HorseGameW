e2f5c713dce6d89f48a39159e442258658c42526
gregorywhytepatriots <49684423+gregorywhytepatriots@users.noreply.github.com>
2019-07-11 11:44:05 -0400

    Update horse-stable.component.spec.ts

--- src/app/horse-stable/horse-stable.component.spec.ts
+++ src/app/horse-stable/horse-stable.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HorseStableComponent } from './horse-stable.component';
 
 describe('HorseStableComponent', () => {
